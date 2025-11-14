292993ea145c8cc9e4f8383caaa40a8a1a7d40af
Chamone Freitas <ch.darley@hotmail.com>
2025-11-14 13:41:00 -0300

    dados de processamento JS

--- calculadoraImc.js
+++ calculadoraImc.js
@@ -2,5 +2,6 @@ function calcular() {
     //dados de entrada
     const peso = document.getElementById("peso")
     const altura = document.getElementById("altura")
-
+    //dados de processamento
+    
 }
