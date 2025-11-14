882b0f5a86729c1a972281a910cae3efe2666877
Chamone Freitas <ch.darley@hotmail.com>
2025-11-14 13:47:19 -0300

    adicionando value

--- calculadoraImc.js
+++ calculadoraImc.js
@@ -1,10 +1,11 @@
 function calcular() {
     //dados de entrada
-    const peso = document.getElementById("peso")
-    const altura = document.getElementById("altura")
+    const peso = document.getElementById("peso").value
+    const altura = document.getElementById("altura").value
     //dados de processamento
     let calculandoImc = peso * (altura/altura)
     let total = calculandoImc
     //saída
-    
+    let resultado = total
+    document.getElementById("resultado").textContent = resultado
 }
